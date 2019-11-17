f997b507e37268b1bf6ad9e587cb2d5ceb82d371
bernard717 <41356074+bernard717@users.noreply.github.com>
2019-11-17 16:23:21 +0900

    Create 1158.java

---
+++ BOJ/1158.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int M = input.nextInt();
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        for(int i = 1; i <= N; i++)
+            queue.offer(i);
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while(N-- > 1){
+            for(int i = 0; i < M - 1; i++){
+                queue.offer(queue.poll());
+            }
+            sb.append(queue.poll());
+            sb.append(", ");
+        }
+        sb.append(queue.poll());
+        sb.append(">");
+
+        System.out.println(sb);
+    }
+}
