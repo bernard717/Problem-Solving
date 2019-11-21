98b1355c2400f360e6f4624612cce2ec2e10c075
bernard717 <41356074+bernard717@users.noreply.github.com>
2019-11-21 23:15:15 +0900

    정렬

---
+++ BOJ/11931.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a =  new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(a);
+        for(int i = n-1; i >= 0; i--)
+            System.out.println(a[i]);
+    }
+}
