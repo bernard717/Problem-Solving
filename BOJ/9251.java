9914d0f12d924b0ba5b3d42a0c11835634c93997
bernard717 <41356074+bernard717@users.noreply.github.com>
2019-11-21 20:16:24 +0900

    dp

---
+++ BOJ/9251.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        int[][] dp = new int[1001][1001];
+        for(int i = 1; i <= a.length(); i++){
+            for(int j = 1; j <= b.length(); j++){
+                if(a.charAt(i - 1) == b.charAt(j - 1))
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        System.out.println(dp[a.length()][b.length()]);
+    }
+}
