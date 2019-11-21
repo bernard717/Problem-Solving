7c3667ba0e3ebcd71b24dee91b24885718b6b1f5
bernard717 <41356074+bernard717@users.noreply.github.com>
2019-11-21 20:34:10 +0900

    구현

---
+++ BOJ/10824.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = bf.readLine().split(" ");
+        String a = line[0] + line[1];
+        String b = line[2] + line[3];
+        long ans = Long.valueOf(a) + Long.valueOf(b);
+        System.out.println(ans);
+    }
+}
