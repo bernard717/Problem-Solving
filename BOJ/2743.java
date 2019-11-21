9ea0ccdb3a43f2dae964999ed7d4d253d62b6f9e
bernard717 <41356074+bernard717@users.noreply.github.com>
2019-11-21 19:56:59 +0900

    문자열 처리

---
+++ BOJ/2743.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        System.out.println(line.length());
+
+    }
+}
