34e7452d21898cfd5d40893cb07fc9ec696c9f33
bernard717 <41356074+bernard717@users.noreply.github.com>
2020-05-01 16:50:24 +0900

    simulation, deque

--- BOJ/3190.java
+++ BOJ/3190.java
@@ -1,91 +1,79 @@
-import java.util.*;
 import java.io.*;
-
-class Main{
-	static int n, answer = 0;
-	static int[][] a;
+import java.util.*;
+class Main {
+	static int N;
+	static int[][] board;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
 	static class Pair{
-		int x;
-		char y;
-		Pair(int x, char y){
-			this.x = x;
-			this.y = y;
-		}
-	}
-	static class Point{
 		int x, y;
-		Point(int x, int y){
+		public Pair(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 	}
-	static Point next(int dir, int x, int y) {
-		if(dir == 0)
-			return new Point(x, y + 1);
-		else if(dir == 1)
-			return new Point(x + 1, y);
-		else if(dir == 2)
-			return new Point(x, y - 1);
-		else
-			return new Point(x - 1, y);
-	}
-	public static void main(String[] args) throws IOException {
+	
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        n = Integer.parseInt(br.readLine());
-        a = new int[n][n];
-        int k = Integer.parseInt(br.readLine());
-        while(k-- > 0) {
-        	String[] line1 = br.readLine().split(" ");
-        	int x = Integer.parseInt(line1[0]);
-        	int y = Integer.parseInt(line1[1]);
-        	a[x - 1][y - 1] = 1;
+        N = Integer.parseInt(br.readLine());
+        
+        board = new int[N][N];
+        
+        int appleNum = Integer.parseInt(br.readLine());
+        for(int i = 0; i < appleNum; i++) {
+        	String[] line = br.readLine().split(" ");
+        	int row = Integer.parseInt(line[0]) - 1;
+        	int col = Integer.parseInt(line[1]) - 1;
+        	board[row][col] = 1;
         }
-        ArrayList<Pair> arr = new ArrayList<Pair>();
-        int l = Integer.parseInt(br.readLine());
-        while(l-- > 0) {
-        	String[] line2 = br.readLine().split(" ");
-        	int x = Integer.parseInt(line2[0]);
-        	char y = line2[1].charAt(0);
-        	arr.add(new Pair(x, y));
+        
+        int turnNum = Integer.parseInt(br.readLine());
+        int[] turning = new int[10001];
+        
+        for(int i = 0; i < turnNum; i++) {
+        	String[] line = br.readLine().split(" ");
+        	int time = Integer.parseInt(line[0]);
+        	char c = line[1].charAt(0);
+        	turning[time] = c == 'L' ? 1 : 2;
         }
-        a[0][0] = 2;
-        int nowX = 0;
-        int nowY = 0;
+        
         int time = 0;
-        int change = 0;
-        int dir = 0;
-        int tailX = 0, tailY = 0;
-        int tailNum = 0;
-        ArrayList<Point> forTail = new ArrayList<Point>();
-        forTail.add(new Point(nowX, nowY));
+        
+        // 시작지점
+        board[0][0] = 2;
+        int dir = 3;
+        Deque<Pair> snake = new LinkedList<>();
+        snake.add(new Pair(0, 0));
+        
+        int[][] turnDir = {{2, 3}, {3, 2}, {1, 0}, {0, 1}};
+        
         while(true) {
-        	if(change < arr.size() && arr.get(change).x == time) {
-        		if(arr.get(change).y == 'L')
-        			dir = (dir + 3) % 4;
-        		else if(arr.get(change).y == 'D')
-        			dir = (dir + 1) % 4;
-        		change++;
+        	// 방향 바꾸는 시간인 경우
+        	if(turning[time] != 0) 
+        		dir = turnDir[dir][turning[time] - 1];
+        	
+        	Pair head = snake.peekFirst();
+        	
+        	int nx = head.x + dx[dir];
+        	int ny = head.y + dy[dir];
+        	
+        	if(nx < 0 || ny < 0 || nx >= N || ny >= N) break;
+        	if(board[nx][ny] == 2) break;
+        	
+        	if(board[nx][ny] == 1) {
+        		board[nx][ny] = 2;
+        		snake.addFirst(new Pair(nx, ny));
         	}
-        	Point p = next(dir, nowX, nowY);
-        	nowX = p.x;
-        	nowY = p.y;
-        	if(nowX < 0 || nowY < 0 || nowX >= n || nowY >= n)
-        		break;
-        	if(a[nowX][nowY] == 2)
-        		break;
-        	forTail.add(new Point(nowX, nowY));
-        	if(a[nowX][nowY] == 1)
-        		a[nowX][nowY] = 2;
-        	else if(a[nowX][nowY] == 0) {
-        		a[nowX][nowY] = 2;
-        		tailX = forTail.get(tailNum).x;
-        		tailY = forTail.get(tailNum).y;
-        		a[tailX][tailY] = 0;
-        		tailNum++;
+        	else {
+        		board[nx][ny] = 2;
+        		snake.addFirst(new Pair(nx, ny));
+        		Pair tail = snake.removeLast();
+        		board[tail.x][tail.y] = 0;
         	}
+        
         	time++;
         }
-        System.out.println(++time);
+        
+        System.out.print(time + 1);
     }
 }
