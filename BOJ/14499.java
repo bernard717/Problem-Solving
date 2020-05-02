c25144a93c97547489f38556f39479c4da22901f
bernard717 <41356074+bernard717@users.noreply.github.com>
2020-05-02 10:15:11 +0900

    simulation

--- BOJ/14499.java
+++ BOJ/14499.java
@@ -1,95 +1,65 @@
-import java.util.*;
+package practice_package;
+
 import java.io.*;
+import java.util.*;
 class Main {
-	static int N, M, K, startX, startY;
-	static int[][] arr;
-	static int[] idx, dice;
-	
-	public static void move(int dir) {
-		if(dir == 1) {
-			int temp1 = idx[4];
-			int temp3 = idx[1];
-			int temp4 = idx[6];
-			int temp6 = idx[3];
-			idx[1] = temp1;
-			idx[3] = temp3;
-			idx[4] = temp4;
-			idx[6] = temp6;
-		}
-		else if(dir == 2) {
-			int temp1 = idx[3];
-			int temp3 = idx[6];
-			int temp4 = idx[1];
-			int temp6 = idx[4];
-			idx[1] = temp1;
-			idx[3] = temp3;
-			idx[4] = temp4;
-			idx[6] = temp6;
-		}
-		else if(dir == 3) {
-			int temp1 = idx[5];
-			int temp2 = idx[1];
-			int temp5 = idx[6];
-			int temp6 = idx[2];
-			idx[1] = temp1;
-			idx[2] = temp2;
-			idx[5] = temp5;
-			idx[6] = temp6;
-		}
-		else if(dir == 4) {
-			int temp1 = idx[2];
-			int temp2 = idx[6];
-			int temp5 = idx[1];
-			int temp6 = idx[5];
-			idx[1] = temp1;
-			idx[2] = temp2;
-			idx[5] = temp5;
-			idx[6] = temp6;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] line = br.readLine().split(" ");
-		N = Integer.parseInt(line[0]);
-		M = Integer.parseInt(line[1]);
-		startX = Integer.parseInt(line[2]);
-		startY = Integer.parseInt(line[3]);
-		K = Integer.parseInt(line[4]);
-		
-		arr = new int[N][M];
-		idx = new int[7];
-		dice = new int[7];
-		
-		for(int i = 0; i < N; i++) {
-			String[] line2 = br.readLine().split(" ");
-			for(int j = 0; j < M; j++)
-				arr[i][j] = Integer.parseInt(line2[j]);
-		}
-		for(int i = 1; i <= 6; i++)
-			idx[i] = i;
-		
-		String[] line3 = br.readLine().split(" ");
-		
-		for(int i = 0; i < K; i++) {
-			int dir = Integer.parseInt(line3[i]);
-			if(dir == 1 && startY < M - 1)
-				startY++;
-			else if(dir == 2 && startY > 0)
-				startY--;
-			else if(dir == 3 && startX > 0)
-				startX--;
-			else if(dir == 4 && startX < N - 1)
-				startX++;
-			else continue;
-			move(dir);
-			if(arr[startX][startY] == 0) 
-				arr[startX][startY] = dice[idx[6]];
-			else {
-				dice[idx[6]] = arr[startX][startY];
-				arr[startX][startY] = 0;
-			}
-			System.out.println(dice[idx[1]]);
-		}
-	}
+	static int[] dx = {0, 0, 0, -1, 1};
+	static int[] dy = {0, 1, -1, 0, 0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int N = Integer.parseInt(line[0]);
+        int M = Integer.parseInt(line[1]);
+        
+        int x = Integer.parseInt(line[2]);
+        int y = Integer.parseInt(line[3]);
+        
+        int K = Integer.parseInt(line[4]);
+        
+        int[][] board = new int[N][M];
+        
+        for(int i = 0; i < N; i++) {
+        	line = br.readLine().split(" ");
+        	for(int j = 0; j < M; j++)
+        		board[i][j] = Integer.parseInt(line[j]);
+        }
+        
+        int[] dir = new int[K];
+        line = br.readLine().split(" ");
+        for(int i = 0; i < K; i++)
+        	dir[i] = Integer.parseInt(line[i]);
+        
+        int[][] diceChange = {{0, 0, 0, 0, 0, 0, 0},
+        		{0, 3, 2, 6, 1, 5, 4},
+        		{0, 4, 2, 1, 6, 5, 3},
+        		{0, 2, 6, 3, 4, 1, 5},
+        		{0, 5, 1, 3, 4, 6, 2}};
+        
+        int[] dice = {0, 0, 0, 0, 0, 0, 0};
+        
+        for(int i = 0; i < K; i++) {
+        	int nowDir = dir[i];
+        	
+        	int nx = x + dx[nowDir];
+        	int ny = y + dy[nowDir];
+        	
+        	if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+        	
+        	x = nx;
+        	y = ny;
+        	
+        	int[] temp = new int[7];
+        	for(int j = 1; j <= 6; j++) 
+        		temp[j] = dice[diceChange[nowDir][j]];
+        	System.arraycopy(temp, 0, dice, 0, temp.length);
+        	
+        	if(board[x][y] == 0) 
+        		board[x][y] = dice[1];
+        	else {
+        		dice[1] = board[x][y];
+        		board[x][y] = 0;
+        	}
+        	System.out.println(dice[6]);
+        }
+    }
 }
