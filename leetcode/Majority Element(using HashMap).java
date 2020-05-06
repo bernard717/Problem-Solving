fbbb618d05b99c8d714363d714340173f114b0fd
bernard717 <41356074+bernard717@users.noreply.github.com>
2020-05-06 14:52:22 +0900

    hashmap

---
+++ leetcode/Majority Element(using HashMap).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])
+                map.put(nums[i], map.get(nums[i]) + 1);
+            else
+                map.put(nums[i], 1);
+        }
+        int standard = (int)Math.ceil(nums.length / 2);
+        for(int key : map.keySet()){
+            if(map.get(key) > standard)
+                return key;
+        }
+        return 0;
+    }
+}
