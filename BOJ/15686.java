ef24946af3589c832321ea6936562130fe0e2083
bernard717 <41356074+bernard717@users.noreply.github.com>
2020-05-08 16:04:16 +0900

    brute force

--- BOJ/15686.java
+++ BOJ/15686.java
@@ -1,123 +1,90 @@
-import java.lang.reflect.Array;
-import java.nio.charset.IllegalCharsetNameException;
-import java.util.*;
 import java.io.*;
-public class Main {
-    static int[][] a;
-    static int[][] dist;
-    static int[][] everyDist;
-    static ArrayList<Integer> house;
-    static ArrayList<Integer> chicken;
-    static int n;
-    static final int[] dx = {0, 0, -1, 1};
-    static final int[] dy = {-1, 1, 0, 0};
-    static class Pair{
-        int x, y;
-        Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    public static boolean next(int[] combi){
-        int i = combi.length - 1;
-        while(i > 0 && combi[i] <= combi[i - 1])
-            i--;
-        if(i <= 0)
-            return false;
-        int j = combi.length - 1;
-        while(combi[j] <= combi[i - 1])
-            j--;
-        int temp = combi[j];
-        combi[j] = combi[i - 1];
-        combi[i - 1] = temp;
-        j = combi.length - 1;
-        while(j > i){
-            temp = combi[j];
-            combi[j] = combi[i];
-            combi[i] = temp;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    public static void bfs(int x, int y){
-        int where = house.indexOf(x * n + y);
-        boolean[][] check = new boolean[n][n];
-        everyDist = new int[n][n];
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(x, y));
-        check[x][y] = true;
-        everyDist[x][y] = 0;
-        while(!q.isEmpty()){
-            Pair p = q.remove();
-            for(int k = 0; k < 4; k++){
-                int nx = p.x + dx[k];
-                int ny = p.y + dy[k];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < n){
-                    if(!check[nx][ny]){
-                        check[nx][ny] = true;
-                        everyDist[nx][ny] = everyDist[p.x][p.y] + 1;
-                        q.add(new Pair(nx, ny));
-                        if(a[nx][ny] == 2)
-                            dist[where][chicken.indexOf(nx * n + ny)] = everyDist[nx][ny];
-                    }
-                }
-            }
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+import java.util.*;
+class Main {	
+	static int N, M; 
+	static int[][] board;
+	static class Pair{
+		int x, y;
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line1 = br.readLine().split(" ");
-        n = Integer.parseInt(line1[0]);
-        int m = Integer.parseInt(line1[1]);
-        a = new int[n][n];
-        int houseNum = 0;
-        int chickenNum = 0;
-        house = new ArrayList<Integer>();
-        chicken = new ArrayList<Integer>();
-        for(int i = 0; i < n; i++){
-            String[] line2 = br.readLine().split(" ");
-            for(int j = 0; j < n; j++) {
-                int what = Integer.parseInt(line2[j]);
-                if(what == 1) {
-                    houseNum++;
-                    house.add(i * n + j);
-                }
-                else if(what == 2) {
-                    chickenNum++;
-                    chicken.add(i * n + j);
-                }
-                a[i][j] = what;
-            }
+        String[] line = br.readLine().split(" ");
+        N = Integer.parseInt(line[0]);
+        M = Integer.parseInt(line[1]);
+         
+        ArrayList<Pair> chickens = new ArrayList<>();
+        ArrayList<Pair> houses = new ArrayList<>();
+        
+        board = new int[N][N];
+        for(int i = 0; i < N; i++) {
+        	line = br.readLine().split(" ");
+        	for(int j = 0; j < N; j++) {
+        		int temp = Integer.parseInt(line[j]);
+        		if(temp == 1)
+        			houses.add(new Pair(i, j));
+        		else if(temp == 2) 
+        			chickens.add(new Pair(i, j));
+        		board[i][j] = temp;
+        	}
         }
-        dist = new int[houseNum][chickenNum];
-        for(Integer p : house)
-            bfs(p / n, p % n);
-        int[] combi = new int[chickenNum];
-        for(int i = 0; i < m; i++)
-            combi[i] = 0;
-        for(int i = m; i < chickenNum; i++)
-            combi[i] = 1;
-        int sum = Integer.MAX_VALUE;
-        do{
-            boolean[] die = new boolean[chickenNum];
-            for(int i = 0; i < chickenNum; i++){
-                if(combi[i] == 1)
-                    die[i] = true;
-            }
-            int min = Integer.MAX_VALUE;
-            int temp = 0;
-            for(int i = 0; i < houseNum; i++){
-                for(int j = 0; j < chickenNum; j++){
-                    if(!die[j])
-                        min = Math.min(min, dist[i][j]);
-                }
-                temp += min;
-                min = Integer.MAX_VALUE;
-            }
-            sum = Math.min(temp, sum);
-        }while(next(combi));
-        System.out.println(sum);
+        
+        int houseNum = houses.size();
+        int chickenNum = chickens.size();
+        
+        int[][] dist = new int[houseNum][chickenNum];
+        
+        for(int i = 0; i < houseNum; i++) {
+        	for(int j = 0; j < chickenNum; j++) {
+        		Pair p = houses.get(i);
+        		Pair q = chickens.get(j);
+        		dist[i][j] = Math.abs(p.x - q.x) + Math.abs(p.y - q.y);
+        	}
+        }
+        
+        int[] perm = new int[chickenNum];
+        for(int i = 0; i < M; i++)
+        	perm[chickenNum - i - 1] = 1;
+        
+        int min = Integer.MAX_VALUE;
+        do {
+        	int sum = 0;
+        	for(int i = 0; i < houseNum; i++) {
+        		int tempMin = Integer.MAX_VALUE;
+        		for(int j = 0; j < chickenNum; j++) {
+        			if(perm[j] == 0) continue;
+        			tempMin = Math.min(tempMin,  dist[i][j]);
+        		}
+        		sum += tempMin;
+        	}
+        	min = Math.min(sum, min);
+        }while(next(perm));
+        
+        System.out.print(min);
     }
+	private static boolean next(int[] a) {
+		int i = a.length - 1;
+		while(i > 0 && a[i - 1] >= a[i])
+			i--;
+		if(i <= 0)
+			return false;
+		int j = a.length - 1;
+		while(a[j] <= a[i - 1])
+			j--;
+		int temp = a[j];
+		a[j] = a[i - 1];
+		a[i - 1] = temp;
+		j = a.length - 1;
+		while(i < j) {
+			temp = a[j];
+			a[j] = a[i];
+			a[i] = temp;
+			j--;
+			i++;
+		}
+		return true;
+	}
 }
