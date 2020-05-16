149c161c58cf8fad7053ff6a0d9550fe81d19cf9
bernard717 <41356074+bernard717@users.noreply.github.com>
2020-05-16 20:49:29 +0900

    sorting

---
+++ HackerRank/Mark and Toys.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+        int cnt = 0;
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    cnt++;
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(String.format("Array is sorted in %d swaps.", cnt));
+        System.out.println(String.format("First Element: %d", a[0]));
+        System.out.println(String.format("Last Element: %d", a[a.length - 1]));
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
